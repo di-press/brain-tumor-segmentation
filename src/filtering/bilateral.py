8acbeb42c4e6755c6371a9e8de80ac27125eade0
math-araujo <araujo_m@usp.br>
2022-06-17 13:52:02 -0300

    Add documentation for bilateral filter

--- src/filtering/bilateral.py
+++ src/filtering/bilateral.py
@@ -1,6 +1,31 @@
+from statistics import mean
 import numpy as np
 
-def bilateral_filter(image, kernel_size, spatial_sigma, intensity_sigma):
+
+def bilateral_filter(image, kernel_size=None, spatial_sigma=1.0, intensity_sigma=None):
+    """
+    Apply the bilateral filter to the input image.
+
+    Args:
+        image (ndarray): input image to be filtered
+        kernel_size (int): size of the filter. If None, this value is set to
+        max(5, 2 * ceil(3 * spatial_sigma) + 1) to replicate the
+        behaviour of skimage denoising_bilateral.
+        spatial_sigma (float): standard deviation for the spatial Gaussian.
+        intensity_sigma (float): standard deviation for the intensity Gaussian.
+        If None, the value is set to image.std() to replicate the behaviour of
+        skimage denoising_bilateral.
+    
+    Returns:
+        processed_image (ndarray): the image after applying the bilateral filter.
+    """
+    # Set the values of the arguments that store None
+    if kernel_size is None:
+        kernel_size = np.max(5, (2 * np.ceil(3 * spatial_sigma)) + 1)
+    if intensity_sigma is None:
+        intensity_sigma = image.std()
+
+    # Pad the input image
     kernel_radius = kernel_size // 2
     image = image.astype(np.float32)
     pad_image = np.pad(image, pad_width=kernel_radius, mode="constant")
@@ -13,6 +38,7 @@ def bilateral_filter(image, kernel_size, spatial_sigma, intensity_sigma):
             weights_sum = 0.0
             current_pixel = pad_image[i, j]
 
+            # Compute the filtered value on the window o
             for m in range(-kernel_radius, kernel_radius+1):
                 for n in range(-kernel_radius, kernel_radius+1):
                     shifted_pixel = pad_image[i + m, j + n]
@@ -28,13 +54,26 @@ def bilateral_filter(image, kernel_size, spatial_sigma, intensity_sigma):
 if __name__ == "__main__":
     from skimage import io
     import matplotlib.pyplot as plt
+    from skimage.filters import gaussian
     from skimage.restoration import denoise_bilateral
+    from skimage.util import random_noise
+    from sklearn.metrics import mean_squared_error
 
-    image = io.imread("dataset/index.jpeg", as_gray=True).astype(np.float32)
-    answer = bilateral_filter(image, kernel_size=5, spatial_sigma=1, intensity_sigma=image.std())
-    skimage_answer = denoise_bilateral(image, win_size=5, sigma_color=image.std())
-    _, axes = plt.subplots(nrows=1, ncols=3)
-    axes[0].imshow(image, cmap="gray")
-    axes[1].imshow(skimage_answer, cmap="gray")
-    axes[2].imshow(answer, cmap="gray")
+    image = io.imread("dataset/test/lena.jpeg", as_gray=True).astype(np.float32)
+    noisy_image = image + random_noise(image)
+    answer = bilateral_filter(noisy_image, kernel_size=5)
+    skimage_answer = denoise_bilateral(noisy_image, win_size=5)
+    _, axes = plt.subplots(nrows=2, ncols=3)
+    axes[0, 0].imshow(noisy_image, cmap="gray")
+    axes[0, 1].imshow(skimage_answer, cmap="gray")
+    axes[0, 2].imshow(answer, cmap="gray")
+
+    gaussian_blurred = gaussian(noisy_image)
+    axes[1, 0].imshow(noisy_image, cmap="gray")
+    axes[1, 1].imshow(gaussian_blurred, cmap="gray")
+    axes[1, 2].imshow(answer, cmap="gray")
     plt.show()
+    
+    print(f"RMSE(input, bilateral) =\t\t{mean_squared_error(image, answer, squared=False)}")
+    print(f"RMSE(input, sk_bilateral) =\t\t{mean_squared_error(image, skimage_answer, squared=False)}")
+    print(f"RMSE(input, gaussian)=\t\t{mean_squared_error(image, gaussian_blurred, squared=False)}")

bcb84529341779e8da785cb358ceeb5f54ef631d
di-press <diany_press@usp.br>
2022-06-18 13:59:26 -0300

    refactor histogram equalization

--- src/enhancement/equalization.py
+++ src/enhancement/equalization.py
@@ -1,8 +1,19 @@
 import numpy as np
 
-def histogram_equalization(image, number_bins=256):
+def histogram_equalization(image, number_of_bins=256):
+    """
+    Compute an image with an equalized histogram
+
+    Args:
+        image (ndarray): image
+        number_of_bins (int): number of bins to use in the histogram.
+    
+    Returns:
+        equalized, cumulative_histogram (ndarray): the equalized version of
+        the input image and the cumulative histogram used to apply the transformation.
+    """
     # Use numpy to compute normalized image histogram
-    histogram, bins = np.histogram(image.flatten(), number_bins, density=True)
+    histogram, bins = np.histogram(image.flatten(), number_of_bins, density=True)
     
     # Compute the cumulative histogram
     cumulative_histogram = histogram.cumsum()

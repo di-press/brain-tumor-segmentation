e93804df4c66296f14946d4be78d79b37917e040
di-press <diany_press@usp.br>
2022-06-18 13:52:51 -0300

    add tests for iou similarity

--- src/evaluation/iou.py
+++ src/evaluation/iou.py
@@ -15,3 +15,28 @@ def iou_similarity_score(ground_truth_mask, predicted):
     intersection = np.logical_and(predicted, ground_truth_mask)
     union = np.logical_or(predicted, ground_truth_mask)
     return np.sum(intersection) / np.sum(union)
+
+if __name__ == "__main__":
+    from sklearn.metrics import jaccard_score
+    
+    mask = np.array([[0, 0, 1], [0, 1, 1], [1, 1, 1]], dtype=np.uint8)
+    image = np.array([[0, 1, 0], [0, 1, 1], [1, 1, 1]], dtype=np.uint8)
+    """
+    Manual computation: 
+    mask =          image =         intersection =      union = 
+    [0, 0, 1]       [0, 1, 0]       [0, 0, 0]           [0, 1, 1]
+    [0, 1, 1]       [0, 1, 1]       [0, 1, 1]           [0, 1, 1]
+    [1, 1, 1]       [1, 1, 1]       [1, 1, 1]           [1, 1, 1]
+    IOU = intersection / union = 5 / 7
+    """
+    skimage_iou = jaccard_score(mask, image, average="micro")
+    iou_score = iou_similarity_score(mask, image)
+    print(f"{iou_score}")
+    assert skimage_iou == iou_score == 5/7
+
+    for size in range(1, 101):
+        mask = np.random.randint(low=0, high=2, size=(size, size))
+        image = np.random.randint(low=0, high=2, size=(size, size))
+        skimage_iou = jaccard_score(mask, image, average="micro")
+        iou_score = iou_similarity_score(mask, image)
+        assert skimage_iou == iou_score

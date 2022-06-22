44dbed97c9c6db68ba2c1fcb01b07a5cb8b66aa6
di-press <diany_press@usp.br>
2022-06-21 22:12:35 -0300

    add Otsu local thresholding function

--- src/segmentation/threshold.py
+++ src/segmentation/threshold.py
@@ -1,3 +1,4 @@
+from tkinter import image_types
 import numpy as np
 
 def stadlbauer_local_thresholding(image):
@@ -24,6 +25,37 @@ def stadlbauer_local_thresholding(image):
     local_threshold = np.mean(image) + (3 * np.std(image))
     segmentation = (image > local_threshold)
     return segmentation
+
+
+
+def otsu_global_thresholding(image, max_intensity=255):
+    """
+    Applies the Otsu's thresholding method. This is an example of
+    global thresholding method adapted from the course material.
+
+    Args:
+        image (ndarray): image to be segmented.
+        max_intensity (int): maximum value of the range of the input image.
+    
+    Returns:
+        segmentation, global_optimum_value (ndarray, int): the predicted segmentation mask and
+        the global optimum value computed by the Otsu's method.
+    """
+    size = np.product(image.shape)
+    weighted_variances = []
+    histogram, _ = np.histogram(image, bins=256, range=(0, 256))
+    image_iteration = (image >= 0).astype(np.uint8)
+    
+    for intensity in np.arange(1, max_intensity):
+        image_iteration = (image >= intensity).astype(np.uint8)
+        weight_a = np.sum(histogram[:intensity]) / float(size)
+        weight_b = np.sum(histogram[intensity:]) / float(size)
+        variance_a = np.var(image[np.where(image_iteration == 0)])
+        variance_b = np.var(image[np.where(image_iteration == 1)])
+        weighted_variances = weighted_variances + [weight_a * variance_a + weight_b * variance_b]
+    global_optimum_value = np.argmin(weighted_variances)
+    segmentation = (image >= global_optimum_value).astype(np.uint8)
+    return segmentation, global_optimum_value
 
 if name == "__main__":
     import matplotlib.pyplot as plt
